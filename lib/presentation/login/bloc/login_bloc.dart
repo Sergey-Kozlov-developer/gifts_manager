e1a6b941bd1f86ce2adba26d88a59d875b6c0dd3
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2022-07-12 17:18:01 +0300

    Login bloc state

--- lib/presentation/login/bloc/login_bloc.dart
+++ lib/presentation/login/bloc/login_bloc.dart
@@ -7,9 +7,10 @@ part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginInitial()) {
-    on<LoginEvent>((event, emit) {
-      // TODO: implement event handler
+  LoginBloc() : super(LoginState.initial()) {
+    // при нажатии кнопки Войти переходим на экран Home
+    on<LoginLoginButtonClicked>((event, emit) {
+      emit(state.copyWith(authenticated: true));
     });
   }
 }
--- lib/presentation/login/bloc/login_state.dart
+++ lib/presentation/login/bloc/login_state.dart
@@ -1,10 +1,50 @@
 part of 'login_bloc.dart';
 
-abstract class LoginState extends Equatable {
-  const LoginState();
-}
+class LoginState extends Equatable {
+  final String email;
+  final String password;
+  final bool emailValid;
+  final bool passwordValid;
+  final bool authenticated;
 
-class LoginInitial extends LoginState {
+  const LoginState({
+    required this.email,
+    required this.password,
+    required this.emailValid,
+    required this.passwordValid,
+    required this.authenticated,
+  });
+
+  // возвращает LoginState
+  factory LoginState.initial() => const LoginState(
+        email: '',
+        password: '',
+        emailValid: false,
+        passwordValid: false,
+        authenticated: false,
+      );
+
+  // метод имутирует состояние пересоздавая с нуля
+  LoginState copyWith({
+    final String? email,
+    final String? password,
+    final bool? emailValid,
+    final bool? passwordValid,
+    final bool? authenticated,
+  }) {
+    // возвращаем новый объект, в котором все поля остаются прежними
+    // но при этом заменяются на новые
+    return LoginState(
+        email: email ?? this.email,
+        password: password ?? this.password,
+        emailValid: emailValid ?? this.emailValid,
+        passwordValid: passwordValid ?? this.passwordValid,
+        authenticated: authenticated ?? this.authenticated,
+    );
+  }
+
   @override
-  List<Object> get props => [];
+  // TODO: implement props
+  List<Object?> get props =>
+      [email, password, emailValid, passwordValid, authenticated];
 }
