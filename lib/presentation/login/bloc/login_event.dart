f8d3305cf62f29ce51ca0efd2510d13d9b81cdab
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2022-07-12 16:19:15 +0300

    Login bloc events

---
+++ lib/presentation/login/bloc/login_event.dart
@@ -0,0 +1,34 @@
+part of 'login_bloc.dart';
+
+abstract class LoginEvent extends Equatable {
+  const LoginEvent();
+}
+
+// Event enter email login
+class LoginEmailChanged extends LoginEvent {
+  final String email;
+
+  const LoginEmailChanged(this.email);
+
+  @override
+  List<Object?> get props => [email];
+}
+
+// Event login password
+class LoginPasswordChanged extends LoginEvent {
+  final String password;
+
+  const LoginPasswordChanged(this.password);
+
+  @override
+  List<Object?> get props => [password];
+}
+
+// Event нажатие на войти
+class LoginLoginButtonClicked extends LoginEvent {
+
+  const LoginLoginButtonClicked();
+
+  @override
+  List<Object?> get props => [];
+}
