2e77e28c6ee9f3656adc67c2dd9fa74dd7440f0c
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2022-07-13 16:26:26 +0300

    Added login state errors

--- lib/presentation/login/bloc/login_state.dart
+++ lib/presentation/login/bloc/login_state.dart
@@ -2,14 +2,18 @@ part of 'login_bloc.dart';
 
 class LoginState extends Equatable {
   final String email;
+  final EmailError emailError;
   final String password;
+  final PasswordError passwordError;
   final bool emailValid;
   final bool passwordValid;
   final bool authenticated;
 
   const LoginState({
     required this.email,
+    required this.emailError,
     required this.password,
+    required this.passwordError,
     required this.emailValid,
     required this.passwordValid,
     required this.authenticated,
@@ -18,7 +22,9 @@ class LoginState extends Equatable {
   // возвращает LoginState
   factory LoginState.initial() => const LoginState(
         email: '',
+        emailError: EmailError.noError,
         password: '',
+        passwordError: PasswordError.noError,
         emailValid: false,
         passwordValid: false,
         authenticated: false,
@@ -27,7 +33,9 @@ class LoginState extends Equatable {
   // метод имутирует состояние пересоздавая с нуля
   LoginState copyWith({
     final String? email,
+    final EmailError? emailError,
     final String? password,
+    final PasswordError? passwordError,
     final bool? emailValid,
     final bool? passwordValid,
     final bool? authenticated,
@@ -35,16 +43,25 @@ class LoginState extends Equatable {
     // возвращаем новый объект, в котором все поля остаются прежними
     // но при этом заменяются на новые
     return LoginState(
-        email: email ?? this.email,
-        password: password ?? this.password,
-        emailValid: emailValid ?? this.emailValid,
-        passwordValid: passwordValid ?? this.passwordValid,
-        authenticated: authenticated ?? this.authenticated,
+      email: email ?? this.email,
+      emailError: emailError ?? this.emailError,
+      password: password ?? this.password,
+      passwordError: passwordError ?? this.passwordError,
+      emailValid: emailValid ?? this.emailValid,
+      passwordValid: passwordValid ?? this.passwordValid,
+      authenticated: authenticated ?? this.authenticated,
     );
   }
 
   @override
   // TODO: implement props
-  List<Object?> get props =>
-      [email, password, emailValid, passwordValid, authenticated];
+  List<Object?> get props => [
+        email,
+        emailError,
+        password,
+        passwordError,
+        emailValid,
+        passwordValid,
+        authenticated,
+      ];
 }
