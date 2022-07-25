fd6ba842e11c2b82ec3454d77fa3ff32e34386c9
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2022-07-25 08:38:40 +0300

    added text fields

--- lib/presentation/registration/bloc/registration_bloc.dart
+++ lib/presentation/registration/bloc/registration_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:bloc/bloc.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:equatable/equatable.dart';
 import 'package:gifts_manager/data/model/request_error.dart';
 import 'package:gifts_manager/presentation/registration/model/errors.dart';
@@ -16,18 +17,59 @@ class RegistrationBloc extends Bloc<RegistrationEvent, RegistrationState> {
 
   String _avatarKey = _defaultAvatarKey;
 
+  // информация о вводе данных email
+  String _email = '';
+  bool _highlightEmailError = false;
+  RegistarationEmailError? _emailError;
+  // информация о вводе данных password
+  String _password = '';
+  bool _highlightPasswordError = false;
+  RegistarationPasswordError? _passwordError;
+
   RegistrationBloc()
       : super(RegistrationFieldsInfo(
           avatarLink: _avatarBuilder(_defaultAvatarKey),
         )) {
-    on<RegistrationChangeAvatar>(onChangeAvatar);
+    on<RegistrationChangeAvatar>(_onChangeAvatar);
+    on<RegistrationEmailChange>(_onEmailChanged);
   }
 
-  FutureOr<void> onChangeAvatar(
+  FutureOr<void> _onChangeAvatar(
     final RegistrationChangeAvatar event,
     final Emitter<RegistrationState> emit,
   ) {
     _avatarKey = Random().nextInt(1000000).toString();
-    emit(RegistrationFieldsInfo(avatarLink: _avatarBuilder(_avatarKey)));
+    emit(_calculateFieldsInfo());
+  }
+
+  FutureOr<void> _onEmailChanged(
+    final RegistrationEmailChange event,
+    final Emitter<RegistrationState> emit,
+  ) {
+    // обновление текущего знач email
+    _email = event.email;
+    // обновление  emailError
+    _emailError = _validateEmail();
+    // эмитим есть ли ошибка или нет. обновляем инфу с филдами
+    emit(_calculateFieldsInfo());
+  }
+
+  // метод для сбора инфы с полей будет выдавать инфу об всех ошибках
+  RegistrationFieldsInfo _calculateFieldsInfo() {
+    return RegistrationFieldsInfo(
+      avatarLink: _avatarBuilder(_avatarKey),
+      emailError: _emailError,
+    );
+  }
+
+  // метод по ошибкам ввода почты
+  RegistarationEmailError? _validateEmail() {
+    if (_email.isEmpty) {
+      return RegistarationEmailError.empty;
+    }
+    if (!EmailValidator.validate(_email)) {
+      return RegistarationEmailError.invalide;
+    }
+    return null;
   }
 }
